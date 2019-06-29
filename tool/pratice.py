e941a0eb0f4483014a902ac1738247ad72b24515
Brother <ltscyt0717@gmail.com>
2019-06-29 21:37:32 +0800

    新增config檔工具

---
+++ tool/pratice.py
@@ -0,0 +1,32 @@
+import os
+
+print('\n\n\t\t新增conf檔參數工具\n\n')
+
+
+title = input('Enter titile=')
+parameter = []
+value = []
+count = 0
+try:
+    while True:
+        parameter.append(input('Enter parameter:'))
+        value.append(input('Enter value:'))
+except EOFError:
+    pass
+
+env = ['dev','prod','uat']
+brand = ['lv','ls','cdd','hy']
+apath=os.path.abspath('.')
+
+for e in env:
+    for b in brand:
+        path = '\\'+e+'\\'+b
+        if not os.path.isdir(apath+path):
+            os.makedirs(apath+path)
+        with open(apath+path+'\\'+'app.conf','w+') as f :
+            f.write('\n')
+            f.write('[' + title + ']' + '\n')
+            for index in range(len(parameter)):
+                f.write(parameter[index] + '=' + value[index] + '\n')
+
+input('新增完畢 按任意鍵結束程式...')
