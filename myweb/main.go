3249db2ea6fb1756fb2796cee087266357a9f11c
fred_chen <fred_chen@paradise-soft.com.tw>
2020-04-15 11:56:47 +0800

    permutations

--- myweb/main.go
+++ myweb/main.go
@@ -9,30 +9,15 @@ import (
 func main() {
 	engine := gin.Default()
 
-	engine.GET("/test/:name", WebRoot)
+	engine.GET("/permutations/:words", WebRoot)
 
 	engine.Run(":9205")
 }
 
 // WebRoot ...
 func WebRoot(c *gin.Context) {
-	str := c.Param("name")
+	words := c.Param("words")
 
-	result := Permutations(str)
+	result := Permutations(words)
 	c.JSON(http.StatusOK, result)
-	// context.String(http.StatusOK, "hello, world %v", "HI")
-}
-
-// Permutations ...
-func Permutations(str string) interface{} {
-	tmp := []string{}
-	for _, s := range str {
-		tmp = append(tmp, string(s))
-	}
-
-	for i := 0; i < len(tmp); i++ {
-		
-	}
-
-	return tmp
 }
